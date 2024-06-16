27ca8ca0e71f533fa65eec02d5a9a918d2c797bb
Basanta Saru <pingazonic@gmail.com>
2024-06-16 16:22:11 +0545

    mongoose connection, node_env set

--- models/owner-model.js
+++ models/owner-model.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const ownerSchema = mongoose.Schema({
-    fullname: String,
+    fullname: {
+        type: String,
+        minLength: 3,
+        trim: true
+    },
     email: String,
     password: String,
     products: {
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+if (process.env.NODE_ENV === "development") {
+    router.post("/create", async function (req, res) {
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res
+                .status(503)
+                .send('You dont have permission to create owner.');
+        }
+        let { fullname, email, password } = req.body;
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        })
+        res.status(201).send(createdOwner);
+    });
+}
 
 router.get("/", function (req, res) {
-    res.send("hey");
+    res.send("hey from owner page");
 });
+
 
 module.exports = router;
